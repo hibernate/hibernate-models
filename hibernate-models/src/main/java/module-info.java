384b6c5aeb1cb9cc5a0ee7bd22c3defb1df877b8
marko-bekhta <marko.prykladna@gmail.com>
2026-02-02 18:54:30 +0100

    Add more exports (for ORM)

--- hibernate-models/src/main/java/module-info.java
+++ hibernate-models/src/main/java/module-info.java
@@ -10,12 +10,12 @@ module org.hibernate.models {
 
 	// TODO: have the "shared classes" in the SPI packages, instead of exporting "internal" packages
 	//  (even if it's to a limited number of own modules) ?
-	exports org.hibernate.models.internal.util to org.hibernate.models.bytebuddy, org.hibernate.models.jandex, org.hibernate.orm.core;
-	exports org.hibernate.models.internal to org.hibernate.models.bytebuddy, org.hibernate.models.jandex, org.hibernate.orm.core;
-	exports org.hibernate.models.internal.jdk to org.hibernate.models.bytebuddy, org.hibernate.models.jandex, org.hibernate.orm.core;
-	exports org.hibernate.models.serial.internal to org.hibernate.models.bytebuddy, org.hibernate.models.jandex, org.hibernate.orm.core;
-	exports org.hibernate.models.internal.dynamic to org.hibernate.orm.core;
-	exports org.hibernate.models.rendering.internal to org.hibernate.orm.core;
+	exports org.hibernate.models.internal.util to org.hibernate.models.bytebuddy, org.hibernate.models.jandex, org.hibernate.orm.core, org.hibernate.orm.envers;
+	exports org.hibernate.models.internal to org.hibernate.models.bytebuddy, org.hibernate.models.jandex, org.hibernate.orm.core, org.hibernate.orm.envers;
+	exports org.hibernate.models.internal.jdk to org.hibernate.models.bytebuddy, org.hibernate.models.jandex, org.hibernate.orm.core, org.hibernate.orm.envers;
+	exports org.hibernate.models.serial.internal to org.hibernate.models.bytebuddy, org.hibernate.models.jandex, org.hibernate.orm.core, org.hibernate.orm.envers;
+	exports org.hibernate.models.internal.dynamic to org.hibernate.orm.core, org.hibernate.orm.envers;
+	exports org.hibernate.models.rendering.internal to org.hibernate.orm.core, org.hibernate.orm.envers;
 
 	uses org.hibernate.models.spi.ModelsContextProvider;
 }
