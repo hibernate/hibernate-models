16cd0d162dc178c70262c935391a2c9b3bb9c508
Steve Ebersole <steve@hibernate.org>
2026-03-07 07:26:11 -0700

    #206 - Add proper module-info descriptors

--- hibernate-models-bytebuddy/src/main/java/module-info.java
+++ hibernate-models-bytebuddy/src/main/java/module-info.java
@@ -5,9 +5,8 @@ module org.hibernate.models.bytebuddy {
 	requires transitive org.hibernate.models;
 
 	exports org.hibernate.models.bytebuddy;
+	// todo : this should all move to "api" (org.hibernate.models.bytebuddy)
 	exports org.hibernate.models.bytebuddy.spi;
-	// exports org.hibernate.models.bytebuddy.internal;
-	// exports org.hibernate.models.bytebuddy.internal.values;
 
 	provides org.hibernate.models.spi.ModelsContextProvider with
 		org.hibernate.models.bytebuddy.internal.ByteBuddyContextProvider;
