b715f463ee9268b08dd4375aa40332f4338afafc
marko-bekhta <marko.prykladna@gmail.com>
2026-08-03 11:00:18 +0200

    Drop build-time generator (at least for now)

--- hibernate-models-accessor/hibernate-models-accessor-generator/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.hibernate.models.accessor.generator {
-	requires org.hibernate.models.accessor;
-	requires org.objectweb.asm;
-
-	exports org.hibernate.models.accessor.generator;
-	exports org.hibernate.models.accessor.generator.runtime;
-}
--- hibernate-models-accessor/hibernate-models-accessor-generator/src/main/java/org/hibernate/models/accessor/generator/GeneratedClassResult.java
+++
@@ -1,8 +0,0 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- * Copyright: Red Hat Inc. and Hibernate Authors
- */
-package org.hibernate.models.accessor.generator;
-
-public record GeneratedClassResult(String className, byte[] bytecode) {
-}
